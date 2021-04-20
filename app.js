96b2368e8797d78932aef517c351e11e24ad5d1a
Mike Lecomte <m.lecomte@gmail.com>
2021-04-20 14:18:20 -0400

    comments for clarity

--- app.js
+++ app.js
@@ -15,16 +15,18 @@ const maxUsers = process.env.MAX_USERS;
 const submissionTimeLimit = process.env.SUBMISSION_TIMER_MS;
 
 const currentUsers = [];
-let userColours = ["Aqua", "Brown", "Coral", "DarkGreen", "DarkMagenta"];
 const userColourMap = new Map();
+let userColours = ["Aqua", "Brown", "Coral", "DarkGreen", "DarkMagenta"];
+
 let turn = 0;
 let submissionId = 0;
 let timer;
 
 let submissions = [];
-let currentSubmission = "";
+let currentSubmission = { text: "" };
 
 const startGame = () => {
+  // Kick off first turn and start the timer
   io.emit("nextTurn", currentUsers[turn]);
   if (!timer) {
     timer = setInterval(nextTurn, submissionTimeLimit);
@@ -32,12 +34,15 @@ const startGame = () => {
 };
 
 const nextTurn = () => {
-  // Add current submission to the array of prior submissions
+  // Add the user's assigned colour and a submissionId to the submission
   currentSubmission.colour = userColourMap.get(currentUsers[turn]);
   currentSubmission.submissionId = submissionId;
+
+  // Add current submission to the array of prior submissions
   submissions.push(currentSubmission);
 
-  currentSubmission = "";
+  // Reset the currentSubmission value to be blank and increment ID for the next user
+  currentSubmission = { text: "" };
   submissionId++;
 
   // Increment turn. If no user exists at this index, start again at 0
@@ -56,6 +61,8 @@ io.on("connection", (client) => {
   // Allow new users to join until the configured max number has been reached
   if (currentUsers.length < maxUsers) {
     currentUsers.push(client.id);
+
+    // Assign the user a unique colour from what's still available
     userColourMap.set(client.id, userColours.pop());
   } else {
     client.emit("error", "Max users reached");
@@ -63,6 +70,7 @@ io.on("connection", (client) => {
     return;
   }
 
+  // Assume that a single user is the start of the game
   if (currentUsers.length === 1) {
     startGame();
   }
@@ -74,6 +82,7 @@ io.on("connection", (client) => {
       currentUsers.splice(index, 1);
     }
 
+    // If we have no users left in our array, reset everything
     if (currentUsers.length === 0) {
       submissions = [];
       clearInterval(timer);
